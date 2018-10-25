07e2060f08a20161f5cf0258a4c9da932c1b1c1c
plaque <haikubee95@gmail.com>
2018-10-25 16:34:21 +0200

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd /
-wget http://kt.agh.edu.pl/~kamisinski/teaching/mpsis/lab/L1/content/clp_1.16_gmpl_ubuntu_16.04.tar.bz2
+sudo wget http://kt.agh.edu.pl/~kamisinski/teaching/mpsis/lab/L1/content/clp_1.16_gmpl_ubuntu_16.04.tar.bz2
 sudo tar -xjvpf clp_1.16_gmpl_ubuntu_16.04.tar.bz2
 export LD_LIBRARY_PATH=/usr/local/lib
